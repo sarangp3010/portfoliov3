2756be145bd0ec06db123a636a7b914e31284920
sarangp3010 <spatel25@albany.edu>
2026-04-18 21:36:50 -0400

    Fix Netlify subdomain asset routing

--- apps/customer/vite.config.ts
+++ apps/customer/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/customer/',
   plugins: [react()],
   server: {
     port: 3002,
